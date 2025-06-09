8535380e342b6ad3a644091d568fb1593aa54f90
aniljangid1111 <aniljangid6199@gmail.com>
2025-06-09 15:37:00 +0530

    hasRountign

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ProductListing from './components/ProductListing'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './CSS/style.css'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router";
 import Home from './components/Home'
 import RootLayout from './components/RootLayout'
 import DetailProduct from './components/DetailProduct'
@@ -16,7 +16,7 @@ import DealsPage from './components/DealsPage'
 
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/e-commerceWeb">
+  <HashRouter basename="/e-commerceWeb">
 
     <Routes>
       <Route element={<RootLayout />}>
@@ -34,24 +34,18 @@ createRoot(document.getElementById('root')).render(
       <Route path='admin-panel'>
 
         <Route path='category'>
-          <Route path='add' element={< Home
-           />} />
-          <Route path='view' element={< Home
-           />} />
-          <Route path='update' element={< Home
-           />} />
+          <Route path='add' element={< Home />} />
+          <Route path='view' element={< Home />} />
+          <Route path='update' element={< Home />} />
         </Route>
 
         <Route path='product'>
-          <Route path='add' element={< Home
-           />} />
-          <Route path='view' element={< Home
-           />} />
-          <Route path='update' element={< Home
-           />} />
+          <Route path='add' element={< Home />} />
+          <Route path='view' element={< Home />} />
+          <Route path='update' element={< Home />} />
         </Route>
 
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
